ab720d3a8a1b231d7f014904ac13fa02058725bb
Sadegh Khalili Tehrani <sadegh.tehraniq@student.kit.edu>
2026-07-10 12:19:37 +0200

    Add metadata to test node

--- src/test_nodes/test_estimator_control/setup.py
+++ src/test_nodes/test_estimator_control/setup.py
@@ -1,29 +1,29 @@
 from setuptools import find_packages, setup
 
-package_name = 'test_estimator_control'
+package_name = "test_estimator_control"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='uoxtx',
-    maintainer_email='uoxtx@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="Sadegh Khalili Tehrani",
+    maintainer_email="sadegh.tehrani@student.kit.edu",
+    description="Test Node for Estimator and Control for Haply shared-control experiments",
+    license="UNLICENSED",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "test_estimator_control_node = test_estimator_control.test_estimator_node:main",
         ],
     },
 )
